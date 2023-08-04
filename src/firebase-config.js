d35e116734a0eba827ac27f929d349757fd6b85e
Shindano Singi <shindanosingi@sekur.com>
2023-08-04 00:51:30 -0400

    User auth is done

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
@@ -15,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import {auth, googleAuthProvider} from '../firebase-config';
+import { signInWithPopup} from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
-	return <div>Login</div>;
+const Login = ({setIsAuth}) => {
+
+	const navigate = useNavigate();
+
+	const signUpWithGoogle = () => {
+		signInWithPopup(auth, googleAuthProvider)
+			.then((result) => {
+				localStorage.setItem('auth', true);
+				setIsAuth(true);
+				navigate('/');
+			})
+	}
+
+	return (
+		<div className="container">
+			<div className="card mt-5 text-center">
+				<div className="card-body">
+					<p className="display-6 mt-3">Sign in with Google</p>
+                              <button className='btn btn-dark' onClick={signUpWithGoogle}>
+                                    Google
+                              </button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
