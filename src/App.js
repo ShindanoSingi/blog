ad33690da8edb7bf27e18a97ad7494a83f1e3dee
Shindano Singi <shindanosingi@sekur.com>
2023-08-03 23:43:11 -0400

    added google auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+
 function App() {
   return (
-      <di>
+      <div>
         <Router>
           <nav className="navbar navbar-expand-lg justify-content-center navbar-light bg-dark text-center py-4">
-            <Link className="nav-link text-white mx-2">Home</Link>
-            <Link className="nav-link text-white mx-2">Create Post</Link>
-            <Link className="nav-link text-white mx-2">Login</Link>
+            <Link to='/' className="nav-link text-white mx-2">Home</Link>
+            <Link to='/create-post' className="nav-link text-white mx-2">Create Post</Link>
+            <Link to='/login' className="nav-link text-white mx-2">Login</Link>
           </nav>
 
           <div className="container mt-5">
-
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create-post" element={<CreatePost />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
           </div>
         </Router>
-      </di>
+      </div>
   );
 }
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,16 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCklXqejW5AFerJCbHFbJEzjZ_J2H6Dp-s",
-  authDomain: "blog-backend-2d425.firebaseapp.com",
-  projectId: "blog-backend-2d425",
-  storageBucket: "blog-backend-2d425.appspot.com",
-  messagingSenderId: "44545276387",
-  appId: "1:44545276387:web:a547ab70ee81798012c8d6"
+	apiKey: 'AIzaSyCklXqejW5AFerJCbHFbJEzjZ_J2H6Dp-s',
+	authDomain: 'blog-backend-2d425.firebaseapp.com',
+	projectId: 'blog-backend-2d425',
+	storageBucket: 'blog-backend-2d425.appspot.com',
+	messagingSenderId: '44545276387',
+	appId: '1:44545276387:web:a547ab70ee81798012c8d6',
 };
 
 // Initialize Firebase
