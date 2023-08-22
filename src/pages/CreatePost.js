5746d77f3cd5a08753f6687fde9265c72d32c588
Shindano Singi <shindanosingi@sekur.com>
2023-08-21 23:48:44 -0400

    Added a text file

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,56 +1,70 @@
-// import React, { useEffect, useState } from 'react';
-// import {addDoc, collection} from "firebase/firestore";
-// import {auth, db} from "../firebase-config";
-// import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { auth, db } from '../firebase-config';
+import { useNavigate } from 'react-router-dom';
 
+const CreatePost = () => {
+	const [title, setTitle] = useState('');
+	const [postTitle, setPostTitle] = useState('');
 
-// const CreatePost = () => {
+	const navigate = useNavigate();
 
-//   const [title, setTitle] = useState('');
-//   const [postTitle, setPostTitle] = useState('');
+	const postsCollectionRef = collection(db, 'posts');
 
-//   const navigate = useNavigate();
+	const createPost = async () => {
+		if (title === '' || postTitle === '') {
+			alert('Please fill all fields');
+			return false;
+		} else {
+			try {
+				await addDoc(postsCollectionRef, {
+					title,
+					postTitle,
+					author: {
+						name: auth.currentUser.displayName,
+						id: auth.currentUser.uid,
+					},
+				});
+				console.log(auth);
+				navigate('/');
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
 
-//   const postsCollectionRef = collection(db, 'posts');
+	return (
+		<div className="container">
+			<div className="bg-light p-5 rounded mt-3">
+				<h1>Create Post</h1>
+				<div className="mb-3">
+					<label htmlFor="title" className="form-label">
+						Title
+					</label>
+					<input
+						type="text"
+						placeholder="Title"
+						className="form-control"
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="posts" className="form-label">
+						Title
+					</label>
+					<textarea
+						className="form-control"
+						placeholder="Write your post here"
+						rows="10"
+						onChange={(e) => setPostTitle(e.target.value)}
+					></textarea>
+				</div>
+				<button className="btn btn-dark" onClick={createPost}>
+					Submit Post
+				</button>
+			</div>
+		</div>
+	);
+};
 
-//   const createPost = async () => {
-//     if(title === '' || postTitle === ''){
-//       alert('Please fill all fields');
-//       return false;
-//     } else{
-//       try{
-//         await addDoc(postsCollectionRef, {
-//           title,
-//           postTitle,
-//           author:{
-//             name: auth.currentUser.displayName,
-//             id: auth.currentUser.uid,
-//           }
-//       });
-//       console.log(auth)
-//       navigate('/');
-//     } catch(error){
-//       console.log(error);
-//     }
-//   };
-// }
-
-//   return (
-//     <div className='container' >
-//       <div className='bg-light p-5 rounded mt-3'>
-//         <h1>Create Post</h1>
-//         <div className="mb-3">
-//           <label htmlFor="title" className='form-label'>Title</label>
-//           <input type="text" placeholder='Title' className='form-control' onChange={(e) => setTitle(e.target.value)} />
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="posts" className='form-label'>Title</label>
-//           <textarea className='form-control' placeholder='Write your post here' rows='10' onChange={(e) => setPostTitle(e.target.value)}></textarea>
-//         </div>
-//         <button className='btn btn-dark' onClick={createPost}>Submit Post</button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default CreatePost
+export default CreatePost;
