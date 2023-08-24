345d2b9560d24d7e88a0cc125a9208ecd70042d7
Shindano Singi <shindanosingi@sekur.com>
2023-08-23 23:42:12 -0400

    Fixed the showEditForm state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+import {
+	getDocs,
+	collection,
+	deleteDoc,
+	doc,
+	updateDoc,
+} from 'firebase/firestore';
 import { auth, db } from '../firebase-config';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+	const [title, setTitle] = useState('');
+	const [postTitle, setPostTitle] = useState('');
+
 	const [postLists, setPostLists] = useState([]);
 	const [loading, setLoading] = useState(false);
 
+	const [showEditForm, setShowEditForm] = useState(false);
+	const [postId, setPostId] = useState('');
+
 	const postsCollectionRef = collection(db, 'posts');
+
+	const navigate = useNavigate();
 
 	// Get all posts
 	const getPosts = async () => {
@@ -16,6 +31,12 @@ function Home() {
 		setLoading(false);
 	};
 
+	// Edit post
+	const editPost = async (id) => {
+		await updateDoc(doc(db, 'posts', id));
+		getPosts();
+	};
+
 	// Delete post
 	const deletePost = async (id) => {
 		await deleteDoc(doc(db, 'posts', id));
@@ -23,40 +44,90 @@ function Home() {
 	};
 
 	useEffect(() => {
+		console.log(showEditForm);
 		getPosts();
+		if (!localStorage.getItem('auth')) {
+			navigate('/login');
+		}
 	}, []);
 
 	return (
 		<div className="homepage">
-			{postLists.length === 0 ? (
-				<h3>No posts added!</h3>
-			) : (
-				postLists.map((post) => {
-					return (
-						<div key={post.id} className="card mb-4 shadow shadow-sm">
-							<div className="d-flex justify-content-end m-3">
-								<button
-									className="btn btn-danger"
-									onClick={() => deletePost(post.id)}
-								>
-									Delete Post
-								</button>
-							</div>
-							<div className="card-body">
-								<h5 className="card-title mb-3 fw-bold">{post.title}</h5>
-								<p className="card-title mb-3">{post.postTitle}</p>
-								<p className="badge bg-dark">{post.author.name}</p>
-							</div>
+			{showEditForm === true ? (
+				<div className="container position-absolute z-index-10">
+					<div className="bg-light p-5 rounded">
+						<h1>Create Post</h1>
+						<div className="mb-3">
+							<label htmlFor="title" className="form-label">
+								Title
+							</label>
+							<input
+								type="text"
+								placeholder="Title"
+								className="form-control"
+								onChange={(e) => setTitle(e.target.value)}
+							/>
 						</div>
-					);
-				})
+						<div className="mb-3">
+							<label htmlFor="posts" className="form-label">
+								Title
+							</label>
+							<textarea
+								className="form-control"
+								placeholder="Write your post here"
+								rows="10"
+								onChange={(e) => setPostTitle(e.target.value)}
+							></textarea>
+						</div>
+						<button
+							className="btn btn-dark"
+							onClick={() => {
+								// editPost(post.id);
+								setShowEditForm(false);
+								// setPostId(post.id);
+								console.log(showEditForm);
+							}}
+						>
+							Submit Post
+						</button>
+					</div>
+				</div>
+			) : (
+				''
 			)}
 
-			{/* <div className="card mb-4 shadow shadow-sm">
-			<div className="card-body">
-				<h1 className="card-title pricing-card-title text-center"></h1>
-			</div>
-		</div> */}
+			{showEditForm === false
+				? postLists.map((post) => {
+						return (
+							<div key={post.id} className="card mb-4 shadow shadow-sm">
+								<div className="d-flex justify-content-end m-3 gap-2">
+									<button
+										className=" btn btn-secondary"
+										onClick={() => {
+											// editPost(post.id);
+											setShowEditForm(true);
+											// setPostId(post.id);
+											console.log(showEditForm);
+										}}
+									>
+										Edit Post
+									</button>
+									<button
+										className="btn btn-danger"
+										onClick={() => deletePost(post.id)}
+									>
+										Delete Post
+									</button>
+								</div>
+								<div className="card-body">
+									<h5 className="card-title mb-3 fw-bold">{post.title}</h5>
+									<p className="card-title mb-3">{post.postTitle}</p>
+									<p className="badge bg-dark">{post.author.name}</p>
+								</div>
+							</div>
+						);
+				  })
+				: ''}
 		</div>
 	);
 }
